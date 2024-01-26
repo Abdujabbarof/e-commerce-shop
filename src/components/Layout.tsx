b0df25ad5cb07341568bd70e067957539a3e43c6
Abdulloh <abdujabborov6172@gmail.com>
2024-01-26 19:24:41 +0500

    first commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout =({children} : {children: ReactNode}) =>{
+    return(
+        <main>
+            <Header />
+            {children}
+            <Footer />
+        </main>
+    )
+}
+
+export default Layout;
