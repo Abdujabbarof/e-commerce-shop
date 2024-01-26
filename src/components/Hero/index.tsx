efe2bb7dd3de272304830438a094fc267ad66157
Abdulloh <abdujabborov6172@gmail.com>
2024-01-26 23:26:16 +0500

    Hero finished

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -11,76 +11,139 @@ import 'swiper/css/thumbs';
 const Hero = () => {  
   return (
     <section className='w-full bg_gradient py-[80px]'>
-      <Swiper className="mySwiper">
+      <Swiper modules={[Pagination, Autoplay, Navigation]} 
+            autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }} 
+          spaceBetween={30}
+          className="mySwiper">
         <SwiperSlide>
-          <div className="main_container grid grid-cols-2">
-            <div className="flex flex-col justify-center items-start gap-y-20 pr-[7%]">
+          <div className="main_container max-w-[708px] lg:max-w-[1280px] flex flex-col gap-y-10 lg:flex-row justify-between gap-[7%]">
+            <div className="flex flex-col justify-center items-start gap-y-[50px] col-span-3">
               <div>
-                <h1 className="leading-[130%] w-full text-[48px] font-bold text-background">Get the Latest Dress Models From Us</h1>
-                <p className="mt-[24px] w-full text-[18px] font-light text-input">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget gravida leo, nec iaculis diam. Nam bibendum mi sed sem finibus ullamcorper.</p>
+                <h1 className="leading-[130%] text-center lg:text-start w-full text-[34px] sm:text-[48px] font-bold text-background">Get the Latest Dress Models From Us</h1>
+                <p className="mt-[24px] w-full text-center lg:text-start text-[18px] font-light text-input">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget gravida leo, nec iaculis diam. Nam bibendum mi sed sem finibus ullamcorper.</p>
               </div>
 
               <div className="flex justify-between w-full">
-                <Button className="text-[18px] font-semibold text-center w-[150px]" variant={'outline'}>Shop now</Button>
+                <Button className="text-[18px] rounded-none font-semibold text-center w-[180px] border-2 text-background p-[22px]" variant={'ghost'}>Shop now</Button>
 
                 <div className="flex flex-col justify-end items-end">
-                  <h2 className="text-[58px] font-bold text-background">30%</h2>
-                  <h2 className="text-[58px] font-bold text-background">OFF</h2>
+                  <h2 className="text-[40px] sm:text-[58px] leading-[100%] font-bold text-background">30%</h2>
+                  <h2 className="text-[40px] sm:text-[58px] leading-[100%] font-bold text-background">OFF</h2>
                 </div>
               </div>
             </div>
 
-            <div className="w-full gap-10 grid grid-cols-3">
-              <div className="w-full col-start-2 col-span-2">
+              <div className="flex justify-center items-center">
                 <Swiper modules={[Pagination, Autoplay, Navigation]} 
                   autoplay={{
-                    delay: 2500,
+                    delay: 3000,
                     disableOnInteraction: false,
                   }} 
                   spaceBetween={30}
                   pagination={{
                     clickable: true,
                   }}
-                  className="mySwiper max-w-[400px] w-full">
+                  className="mySwiper max-w-[400px]">
                   <SwiperSlide>
-                    <img src="/cap1.JPG" alt="Hero Image" className="border-[10px] rounded-[100px] rounded-es-none" />
+                    <img src="/cap1.JPG" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
                   </SwiperSlide>
                   <SwiperSlide>
-                    <img src="/cap2.JPG" alt="Hero Image" className="border-[10px] rounded-[100px] rounded-es-none" />
+                    <img src="/cap2.JPG" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src="/cap3.jpeg" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
                   </SwiperSlide>
                 </Swiper>
               </div>
+          </div>
+        </SwiperSlide>
 
-              <div className="flex flex-col justify-between items-center row-start-1 col-span-1">
+        <SwiperSlide>
+          <div className="main_container max-w-[708px] lg:max-w-[1280px] flex flex-col gap-y-10 lg:flex-row justify-between gap-[7%]">
+            <div className="flex flex-col justify-center items-start gap-y-[50px] col-span-3">
+              <div>
+                <h1 className="leading-[130%] text-center lg:text-start w-full text-[34px] sm:text-[48px] font-bold text-background">Get the Latest Dress Models From Us</h1>
+                <p className="mt-[24px] w-full text-center lg:text-start text-[18px] font-light text-input">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget gravida leo, nec iaculis diam. Nam bibendum mi sed sem finibus ullamcorper.</p>
+              </div>
+
+              <div className="flex justify-between w-full">
+                <Button className="text-[18px] rounded-none font-semibold text-center w-[180px] border-2 text-background p-[22px]" variant={'ghost'}>Shop now</Button>
+
+                <div className="flex flex-col justify-end items-end">
+                  <h2 className="text-[40px] sm:text-[58px] leading-[100%] font-bold text-background">30%</h2>
+                  <h2 className="text-[40px] sm:text-[58px] leading-[100%] font-bold text-background">OFF</h2>
+                </div>
+              </div>
+            </div>
+
+              <div className="flex justify-center items-center">
                 <Swiper modules={[Pagination, Autoplay, Navigation]} 
                   autoplay={{
-                    delay: 2500,
+                    delay: 3000,
                     disableOnInteraction: false,
                   }} 
                   spaceBetween={30}
-                  navigation
-                  className="mySwiper max-w-[400px] w-full h-[50%]">
-                  <SwiperSlide className="w-full object-fill flex justify-center items-center">
-                    <img src="/cap3.jpeg" alt="Hero Image" />
+                  pagination={{
+                    clickable: true,
+                  }}
+                  className="mySwiper max-w-[400px]">
+                  <SwiperSlide>
+                    <img src="/cap1.JPG" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
                   </SwiperSlide>
-                  <SwiperSlide className="w-full object-cover">
-                    <img src="/cap4.jpeg" alt="Hero Image" />
+                  <SwiperSlide>
+                    <img src="/cap2.JPG" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src="/cap3.jpeg" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
                   </SwiperSlide>
                 </Swiper>
               </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="main_container max-w-[708px] lg:max-w-[1280px] flex flex-col gap-y-10 lg:flex-row justify-between gap-[7%]">
+            <div className="flex flex-col justify-center items-start gap-y-[50px] col-span-3">
+              <div>
+                <h1 className="leading-[130%] text-center lg:text-start w-full text-[34px] sm:text-[48px] font-bold text-background">Get the Latest Dress Models From Us</h1>
+                <p className="mt-[24px] w-full text-center lg:text-start text-[18px] font-light text-input">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget gravida leo, nec iaculis diam. Nam bibendum mi sed sem finibus ullamcorper.</p>
+              </div>
+
+              <div className="flex justify-between w-full">
+                <Button className="text-[18px] rounded-none font-semibold text-center w-[180px] border-2 text-background p-[22px]" variant={'ghost'}>Shop now</Button>
+
+                <div className="flex flex-col justify-end items-end">
+                  <h2 className="text-[40px] sm:text-[58px] leading-[100%] font-bold text-background">30%</h2>
+                  <h2 className="text-[40px] sm:text-[58px] leading-[100%] font-bold text-background">OFF</h2>
+                </div>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="main_container">
-            <h1 className="max-w-[426px] w-full text-[48px] font-bold text-background">Get the Latest Dress Models From Us</h1>
-            <p className="mt-[24px] max-w-[508px] w-full text-[18px] font-normal text-input">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget gravida leo, nec iaculis diam. Nam bibendum mi sed sem finibus ullamcorper.</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="main_container">
-            <h1 className="max-w-[426px] w-full text-[48px] font-bold text-background">Get the Latest Dress Models From Us</h1>
-            <p className="mt-[24px] max-w-[508px] w-full text-[18px] font-normal text-input">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget gravida leo, nec iaculis diam. Nam bibendum mi sed sem finibus ullamcorper.</p>
+
+              <div className="flex justify-center items-center">
+                <Swiper modules={[Pagination, Autoplay, Navigation]} 
+                  autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                  }} 
+                  spaceBetween={30}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  className="mySwiper max-w-[400px]">
+                  <SwiperSlide>
+                    <img src="/cap1.JPG" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src="/cap2.JPG" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img src="/cap3.jpeg" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
+                  </SwiperSlide>
+                </Swiper>
+              </div>
           </div>
         </SwiperSlide>
       </Swiper>
