0e1532e25e78c094e3f937483ee1fad48ec163e5
Abdulloh <abdujabborov6172@gmail.com>
2024-01-26 23:28:39 +0500

    Hero finished

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -39,7 +39,7 @@ const Hero = () => {
               <div className="flex justify-center items-center">
                 <Swiper modules={[Pagination, Autoplay, Navigation]} 
                   autoplay={{
-                    delay: 3000,
+                    delay: 2000,
                     disableOnInteraction: false,
                   }} 
                   spaceBetween={30}
@@ -82,7 +82,7 @@ const Hero = () => {
               <div className="flex justify-center items-center">
                 <Swiper modules={[Pagination, Autoplay, Navigation]} 
                   autoplay={{
-                    delay: 3000,
+                    delay: 2000,
                     disableOnInteraction: false,
                   }} 
                   spaceBetween={30}
@@ -91,56 +91,10 @@ const Hero = () => {
                   }}
                   className="mySwiper max-w-[400px]">
                   <SwiperSlide>
-                    <img src="/cap1.JPG" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
+                    <img src="/hero1.jpeg" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
                   </SwiperSlide>
                   <SwiperSlide>
-                    <img src="/cap2.JPG" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <img src="/cap3.jpeg" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
-                  </SwiperSlide>
-                </Swiper>
-              </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="main_container max-w-[708px] lg:max-w-[1280px] flex flex-col gap-y-10 lg:flex-row justify-between gap-[7%]">
-            <div className="flex flex-col justify-center items-start gap-y-[50px] col-span-3">
-              <div>
-                <h1 className="leading-[130%] text-center lg:text-start w-full text-[34px] sm:text-[48px] font-bold text-background">Get the Latest Dress Models From Us</h1>
-                <p className="mt-[24px] w-full text-center lg:text-start text-[18px] font-light text-input">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget gravida leo, nec iaculis diam. Nam bibendum mi sed sem finibus ullamcorper.</p>
-              </div>
-
-              <div className="flex justify-between w-full">
-                <Button className="text-[18px] rounded-none font-semibold text-center w-[180px] border-2 text-background p-[22px]" variant={'ghost'}>Shop now</Button>
-
-                <div className="flex flex-col justify-end items-end">
-                  <h2 className="text-[40px] sm:text-[58px] leading-[100%] font-bold text-background">30%</h2>
-                  <h2 className="text-[40px] sm:text-[58px] leading-[100%] font-bold text-background">OFF</h2>
-                </div>
-              </div>
-            </div>
-
-              <div className="flex justify-center items-center">
-                <Swiper modules={[Pagination, Autoplay, Navigation]} 
-                  autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                  }} 
-                  spaceBetween={30}
-                  pagination={{
-                    clickable: true,
-                  }}
-                  className="mySwiper max-w-[400px]">
-                  <SwiperSlide>
-                    <img src="/cap1.JPG" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <img src="/cap2.JPG" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <img src="/cap3.jpeg" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
+                    <img src="/hero2.jpeg" alt="Hero Image" className="border-[10px] mx-auto w-[300px] h-[391px] sm:w-[400px] sm:h-[522px] object-cover rounded-[100px] rounded-es-none" />
                   </SwiperSlide>
                 </Swiper>
               </div>
